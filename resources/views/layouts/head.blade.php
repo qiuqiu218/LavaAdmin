97444fff62aae0e3e0fc419cc178f500c3793c43
万鑫 <295686243@qq.com>
2017-12-31 22:16:42 +0800

    公共样式

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,3 +1,3 @@
 <link rel="stylesheet" href="{{ asset('vendor/layui/css/layui.css') }}">
-<link rel="stylesheet" href="{{ asset('css/vendor.css') }}?v=0.0.1">
+<link rel="stylesheet" href="{{ asset('css/vendor.css') }}?v=0.0.2">
 {!! $cssPath !!}
