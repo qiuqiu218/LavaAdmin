093f1da440a91d9578b2b71633ac60f74b497dbd
万鑫 <295686243@qq.com>
2018-01-01 22:23:26 +0800

    后台主页

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -3,24 +3,57 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\BaseController;
+use App\Models\Admin\Menu;
+use Illuminate\Http\Request;
 
 class IndexController extends BaseController
 {
     /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function index()
     {
-        return view('admin.index.index');
+        $data = Menu::query()
+                    ->whereNull('parent_id')
+                    ->orderBy('sort', 'asc')
+                    ->orderBy('id', 'asc')
+                    ->get();
+        return view('admin.index.index', [
+            'data' => $data
+        ]);
     }
 
     /**
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function menu()
+    public function main()
     {
-        return view('admin.index.menu');
+        return view('admin.index.main');
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function sidebar(Request $request)
+    {
+        $data = array();
+        $parent_id = intval($request->input('parent_id'));
+        if (!$parent_id) {
+            $first = Menu::query()
+                            ->whereNull('parent_id')
+                            ->orderBy('sort', 'asc')
+                            ->orderBy('id', 'asc')
+                            ->first();
+            if ($first) {
+                $parent_id = $first->id;
+            }
+        }
+        if ($parent_id) {
+            $data = Menu::findOrFail($parent_id)->getDescendants()->toHierarchy();
+        }
+        return view('admin.index.sidebar', [
+            'data' => $data
+        ]);
     }
 }
---
+++ resources/views/admin/index/main.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+
+@section('content')
+<div>首页</div>
+@endsection
