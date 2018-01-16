0ffda115eff74cff73a272fbc38fb80b6ebb25ad
DESKTOP-33F4D6L\万鑫 <295686243@qq.com>
2018-01-16 18:15:50 +0800

    编译更新

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -1,3 +1,3 @@
 <script src="{{ asset('js/vendor.js') }}"></script>
-<script src="{{ asset('vendor/layui/layui.js') }}"></script>
+<script src="{{ asset('vendor/layui/layui.all.js') }}"></script>
 {!! $jsPath !!}
